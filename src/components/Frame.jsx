dfc56cbdbc82a214c8471722fbcaa7f317e88d3d
Tahmeed <ec24374@qmul.ac.uk>
2026-04-01 04:19:41 +0100

    Made it fit content -- bugfix

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,9 +1,9 @@
 import styles from "./Frame.module.css";
 
-function Frame({ theme, minSize, maxSize, children }) {
+function Frame({ theme, fitContent, children }) {
 // might add minSize and maxSize later, if not then remove this
     return (
-        <div className={`${styles.frame} ${styles[theme]}`}> 
+        <div className={`${styles.frame} ${styles[theme]}`} style ={{width : fitContent ? "fit-content" : undefined}}>
             {children}
         </div>
     );
